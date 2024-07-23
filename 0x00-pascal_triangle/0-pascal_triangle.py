e639bb4fcbce719dc4c0796c87f9d0be5489209b
rekebuba <abukiabdullahi1@gmail.com>
2024-07-23 14:46:37 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle:
+    returns a list of lists of integers representing the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    result = []
+
+    for i in range(n - 1):
+        for j in range(len(triangle[i])):
+            try:
+                result.append(triangle[i][j] + triangle[i][j + 1])
+            except IndexError:
+                pass
+
+        result.insert(0, 1)
+        result.append(1)
+        triangle.append(result)
+        result = []
+
+    return triangle
