a660d175b77c4e137efad5fb3205ed5518dc1cfa
rekebuba <abukiabdullahi1@gmail.com>
2024-07-26 02:12:21 +0300

    debugging

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,15 +14,12 @@ def pascal_triangle(n):
 
     triangle = [[1]]
 
-    for i in range(n - 1):
-        result = []
-        for j in range(len(triangle[i])):
-            try:
-                result.append(triangle[i][j] + triangle[i][j + 1])
-            except IndexError:
-                result.insert(0, 1)
-                result.append(1)
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
 
-        triangle.append(result)
+        row.append(1)
+        triangle.append(row)
 
     return triangle
