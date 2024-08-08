bd223e7844eb36cb8e2bed04b8629e1bdbff04e5
rekebuba <abukiabdullahi1@gmail.com>
2024-08-08 23:56:48 +0300

    0x02-minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python3
+"""Minimum Number of Copy / Paste Operations"""
 
 
-def minOperations(n) -> int:
-    
-    return 0
+def minOperations(n: int) -> int:
+    """calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+
+    Args:
+        n (int)
+
+    Returns:
+        int: minimum number of operations needed
+        to make the resulting string exactly of length n.
+    """
+    if n == 1:
+        return 0
+
+    copy = 1
+    char = 2
+    operations = 2  # initial operation
+
+    while char != n:
+        if n % char == 0:
+            copy = char  # coping the characters
+            operations += 1
+
+        char += copy  # pasting the copied characters
+        operations += 1
+
+    return operations
