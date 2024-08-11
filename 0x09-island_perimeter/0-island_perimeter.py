063336c5ab0dd74ee5e84dfe5170d08e627588ba
rekebuba <abukiabdullahi1@gmail.com>
2024-08-11 23:51:56 +0300

    0x09-island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from typing import List
+
+def productExceptSelf(nums: List[int]) -> List[int]:
+    result = [1] * len(nums)
+    for i, num in enumerate(nums):
+        for j, x in enumerate(result):
+            if j != i:
+                result *= x 
+
+    return result
+
+result = productExceptSelf([1,2,3,4])
+print(result)
