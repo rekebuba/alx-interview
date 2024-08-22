f0581e8881b3b254a48a9abd64b0fce3ac0b1927
rekebuba <abukiabdullahi1@gmail.com>
2024-08-22 22:43:49 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -19,19 +19,19 @@ def validUTF8(data: List[int]) -> bool:
     i = 0
     while valid and i < len(binary):
         try:
-            bytes = binary[i].index('0') - 1
+            byte = binary[i].index('0') - 1
         except ValueError:
-            bytes = 0
+            byte = 0
 
-        if 4 > bytes > 0:
+        if 4 > byte > 0:
             i += 1
-            while valid and bytes > 0:
+            while valid and byte > 0:
                 try:
                     valid = binary[i][:2] == '10'
                 except IndexError:
                     valid = False
                 i += 1
-                bytes -= 1
+                byte -= 1
         else:
             valid = binary[i][0] == '0'
             i += 1
