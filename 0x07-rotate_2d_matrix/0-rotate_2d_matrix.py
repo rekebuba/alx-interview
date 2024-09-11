a13ff7d7626c2daac9f4daf13f98eb80df4fff44
rekebuba <abukiabdullahi1@gmail.com>
2024-09-11 23:37:07 +0300

    0x07-rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """Rotate 2D Matrix"""
-from typing import List
 
 
-def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+def rotate_2d_matrix(matrix) -> None:
     """Dose not return anything, modifies matrix in-place
     by rotating it 90 degrees clockwise.
 
