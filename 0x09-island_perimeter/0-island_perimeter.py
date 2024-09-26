0340402d9fb4b7757da3de0cbc139df3a797de7e
rekebuba <abukiabdullahi1@gmail.com>
2024-09-27 01:23:10 +0300

    0x09-island_perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-from typing import List
+"""Island perimeter"""
 
-def productExceptSelf(nums: List[int]) -> List[int]:
-    result = [1] * len(nums)
-    for i, num in enumerate(nums):
-        for j, x in enumerate(result):
-            if j != i:
-                result *= x 
 
-    return result
-
-result = productExceptSelf([1,2,3,4])
-print(result)
+def island_perimeter(grid):
+    """perimeter of an island with no lakes"""
+    perimeter = 0
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return perimeter
